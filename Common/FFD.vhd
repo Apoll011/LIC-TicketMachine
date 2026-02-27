1a9288cf44d64d005dcc5b30cfadf4cd3e07d9fa
apoll011 <tiagorobotik@gmail.com>
2026-02-27 11:33:12 +0000

    style(FFD): Format VHDL code for improved readability and consistencyFormats the FFD entity and architecture by:- Standardizing keywords to lowercase.- Adding consistent indentation to port declarations.- Removing unnecessary blank lines

--- Common/FFD.vhd
+++ Common/FFD.vhd
@@ -1,22 +1,19 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
-ENTITY FFD IS
-PORT(	CLK : in std_logic;
+entity FFD IS
+	port(	
+		CLK 	: in std_logic;
 		RESET : in STD_LOGIC;
-		SET : in std_logic;
-		D : IN STD_LOGIC;
-		EN : IN STD_LOGIC;
-		Q : out std_logic
-		);
-END FFD;
+		SET 	: in std_logic;
+		D 		: IN STD_LOGIC;
+		EN 	: IN STD_LOGIC;
+		Q 		: out std_logic
+	);
+end FFD;
 
-ARCHITECTURE logicFunction OF FFD IS
+architecture logicFunction of FFD is
 
-BEGIN
-
-
-Q <= '0' when RESET = '1' else '1' when SET = '1' else D WHEN rising_edge(clk) and EN = '1';
-
-
-END LogicFunction;
+begin
+	Q <= '0' when RESET = '1' else '1' when SET = '1' else D WHEN rising_edge(clk) and EN = '1';
+end LogicFunction;
