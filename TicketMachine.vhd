17a7c345e6b144a6952db0969529c5818265bf5b
Tomasmsf <tmsf3005@gmail.com>
2026-03-06 09:52:27 +0000

    no message

--- TicketMachine.vhd
+++ TicketMachine.vhd
@@ -3,8 +3,8 @@ USE ieee.std_logic_1164.all;
 
 entity TicketMachine is
 	port(	
-		inputPortT:  IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
-		outputPortT :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
+		inputPort:  IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
+		outputPort :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
 	);
 end TicketMachine;
 
@@ -16,12 +16,13 @@ architecture logicFunction of TicketMachine is
 		outputPort :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
 	);
 	end component;
-	
+		
 begin
 
 	usb: UsbPort port map (
-		inputPort 	=> inputPortT,
-		outputPort 	=> outputPortT
+		inputPort 	=> inputPort,
+		outputPort 	=> outputPort
 	);
+	
 	
 end logicFunction;
