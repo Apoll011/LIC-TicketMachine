3ac8715203eb605a051eb1e33dd5bd2addceb0e0
apoll011 <tiagorobotik@gmail.com>
2026-03-06 11:32:41 +0000

    refactor(vhdl): integrate Key_Decode component

--- TicketMachine.vhd
+++ TicketMachine.vhd
@@ -3,26 +3,33 @@ USE ieee.std_logic_1164.all;
 
 entity TicketMachine is
 	port(	
-		inputPort:  IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
-		outputPort :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
+		RESET, CLK  	: in std_logic;
+		Kval								: out std_logic;
+		K 									: out std_logic_vector(3 downto 0);
+		Keys_Vertical 					: out std_logic_vector(3 downto 0);
+		Keys_Horizontal				: in std_logic_vector(3 downto 0)
 	);
 end TicketMachine;
 
 architecture logicFunction of TicketMachine is
-	
-	component UsbPort
+	component Key_Decode
 	port( 
-		inputPort:  IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
-		outputPort :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
+		Kack, Tdelay, RESET, CLK  	: in std_logic;
+		Kval								: out std_logic;
+		K 									: out std_logic_vector(3 downto 0);
+		Keys_Vertical 					: out std_logic_vector(3 downto 0);
+		Keys_Horizontal				: in std_logic_vector(3 downto 0)
 	);
 	end component;
-		
 begin
-
-	usb: UsbPort port map (
-		inputPort 	=> inputPort,
-		outputPort 	=> outputPort
+	decode: Key_Decode port map (
+		Kack => 1,
+		Tdelay => 0,
+		RESET => RESET,
+		CLK => CLK,
+		Kval => Kval,
+		K => K,
+		Keys_Vertical => Keys_Vertical,
+		Keys_Horizontal => Keys_Horizontal
 	);
-	
-	
 end logicFunction;
