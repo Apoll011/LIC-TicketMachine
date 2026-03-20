503d0f18c6b38b97731986145a35904250316267
Tomasmsf <tmsf3005@gmail.com>
2026-03-20 10:50:48 +0000

    no message

--- TicketMachine.vhd
+++ TicketMachine.vhd
@@ -14,7 +14,7 @@ end TicketMachine;
 architecture logicFunction of TicketMachine is
 	component Key_Decode
 	port( 
-		Kack, RESET, CLK  	: in std_logic;
+		Kack, RESET, CLK  			: in std_logic;
 		Tdelay 							: in std_logic_vector(1 downto 0);
 		Kval								: out std_logic;
 		K 									: out std_logic_vector(3 downto 0);
@@ -41,7 +41,7 @@ begin
 
 	decode: Key_Decode port map (
 		Kack => outputPort(0),
-		Tdelay =>"11",
+		Tdelay => "11",
 		RESET => RESET,
 		CLK => CLK,
 		K(0) => inputPort(6),
