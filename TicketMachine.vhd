e89703fb0a0fad2f4d39df1fb76baab5ff96cda3
Apoll0_11 <80171544+Apoll011@users.noreply.github.com>
2026-04-01 23:10:20 +0100

    Update TicketMachine.vhd

--- TicketMachine.vhd
+++ TicketMachine.vhd
@@ -1,74 +1,89 @@
-
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
 entity TicketMachine is
-	port(	
-		Kval,Kscan : out std_logic;
-		CLK, RESET 						: in std_logic;
-		Keys_Vertical					: out std_logic_vector(3 downto 0);
-		Keys_Horizontal				: in std_logic_vector(3 downto 0);
-		LCD_DATA		           		: out std_logic_vector(9 downto 0)
-	);
-end TicketMachine;
+    port (
+        CLK             : in  std_logic;
+        RESET           : in  std_logic;
+
+        Keys_Vertical   : out std_logic_vector(3 downto 0);  -- column drives
+        Keys_Horizontal : in  std_logic_vector(3 downto 0);  -- row reads
+
+        LCD_RS          : out std_logic;                      -- register select
+        LCD_EN          : out std_logic;                      -- enable strobe
+        LCD_DATA        : out std_logic_vector(7 downto 0)    -- data[7:0]
+    );
+end entity TicketMachine;
 
 architecture logicFunction of TicketMachine is
-	component Key_Decode
-	port( 
-		Kack, RESET, CLK  			: in std_logic;
-		Tdelay 							: in std_logic_vector(1 downto 0);
-		Kval								: out std_logic;
-		K 									: out std_logic_vector(3 downto 0);
-		Keys_Vertical 					: out std_logic_vector(3 downto 0);
-		Keys_Horizontal				: in std_logic_vector(3 downto 0)
-	);
-	end component;
-	
-	component UsbPort
-	port( 
-		inputPort	:  IN  STD_LOGIC_VECTOR(7 DOWNTO 0);
-		outputPort 	:  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
-	);
-	end component;
-	
-	component SerialReceiver
-    port(
-        SDX, CLK, SS, RESET   : in  std_logic;
-        Q           				: out std_logic_vector(9 downto 0)
-    );
-	end component;
+    component Key_Decode is
+        port (
+            Kack, RESET, CLK : in  std_logic;
+            Tdelay           : in  std_logic_vector(1 downto 0);
+            Kval             : out std_logic;
+            K                : out std_logic_vector(3 downto 0);
+            Keys_Vertical    : out std_logic_vector(3 downto 0);
+            Keys_Horizontal  : in  std_logic_vector(3 downto 0)
+        );
+    end component Key_Decode;
 
-	signal inputPort, outputPort	: STD_LOGIC_VECTOR(7 DOWNTO 0);	
+    component UsbPort is
+        port (
+            inputPort  : in  std_logic_vector(7 downto 0);
+            outputPort : out std_logic_vector(7 downto 0)
+        );
+    end component UsbPort;
+
+    component SerialReceiver is
+        port (
+            SDX, CLK, SS, RESET : in  std_logic;
+            Q                   : out std_logic_vector(9 downto 0)
+        );
+    end component SerialReceiver;
+
+  	signal inputPort  : std_logic_vector(7 downto 0);
+    signal outputPort : std_logic_vector(7 downto 0);
+
+    signal key_code   : std_logic_vector(3 downto 0);
+    signal kval_int   : std_logic;
+
+    signal lcd_frame  : std_logic_vector(9 downto 0);
+
 begin
 
-	usb: UsbPort port map (
+    usb: component UsbPort
+    port map (
+        inputPort  => inputPort,
+        outputPort => outputPort
+    );
 
-		inputPort 	=> inputPort,
-		outputPort 	=> outputPort
-	);
-	Kval <= outputPort(7);
-	decode: Key_Decode port map (
-		Kack => outputPort(7),
-		Tdelay => "11",
-		RESET => RESET,
-		CLK => CLK,
-		K(0) => inputPort(0),
-		K(1) => inputPort(1),
-		K(2) => inputPort(2),
-		K(3) => inputPort(3),
-		Kval => inputPort(7),
-		Keys_Vertical => Keys_Vertical,
-		Keys_Horizontal => Keys_Horizontal
-	);
-	
-	lcd_serial : SerialReceiver port map (
-		SDX 	=> outputPort(0),
-		CLK 	=> outputPort(1),
-		SS  	=> outputPort(2),
-		Q 	 	=> LCD_DATA,
-		RESET => RESET
-	);
-	
-	
-	
-end logicFunction;
+    decode: component Key_Decode
+    port map (
+        RESET           => RESET,
+        CLK             => CLK,
+        Kack            => inputPort(7),    -- software sets bit7 to ack a key
+        Tdelay          => "11",
+        Kval            => kval_int,
+        K               => key_code,
+        Keys_Vertical   => Keys_Vertical,
+        Keys_Horizontal => Keys_Horizontal
+    );
+
+    outputPort(7)          <= kval_int;
+    outputPort(6 downto 4) <= "000"; 
+    outputPort(3 downto 0) <= key_code;
+
+    lcd_serial: component SerialReceiver
+    port map (
+        RESET => RESET,
+        CLK   => inputPort(1),   -- serial clock driven by software (SCLK_MASK bit 1)
+        SDX   => inputPort(0),   -- serial data  (SDX_MASK  bit 0)
+        SS    => inputPort(2),   -- slave select (LCD_MASK   bit 2)
+        Q     => lcd_frame
+    );
+
+    LCD_RS   <= lcd_frame(0);              -- bit 0  = RS
+    LCD_DATA <= lcd_frame(8 downto 1);     -- bits 8:1 = data byte
+    LCD_EN   <= lcd_frame(9);              -- bit 9  = E (enable)
+
+end architecture logicFunction;
