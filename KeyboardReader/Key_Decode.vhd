1c9633f88cb5a144a7dc79df1f31a91900ed5b0e
apoll011 <tiagorobotik@gmail.com>
2026-04-27 09:22:44 +0100

    new key control

--- KeyboardReader/Key_Decode.vhd
+++ KeyboardReader/Key_Decode.vhd
@@ -5,7 +5,7 @@ entity Key_Decode is
     port (
         Kack, RESET, CLK : in  std_logic;
         Tdelay           : in  std_logic_vector(1 downto 0);
-        Kval		       : out std_logic;
+        Kval             : out std_logic;
         K                : out std_logic_vector(3 downto 0);
         Keys_Vertical    : out std_logic_vector(3 downto 0);
         Keys_Horizontal  : in  std_logic_vector(3 downto 0)
@@ -16,9 +16,9 @@ architecture logicFunction of Key_Decode is
 
     component Key_Control is
         port (
-            Kack, Kpress, RESET, CLK : in  std_logic;
+            CLK, RESET, Kack, Kpress : in  std_logic;
             Tdelay                   : in  std_logic_vector(1 downto 0);
-            Kval, Kscan			       : out std_logic
+            Kval, Kscan              : out std_logic
         );
     end component Key_Control;
 
@@ -34,48 +34,45 @@ architecture logicFunction of Key_Decode is
 
     component CLKDIV is
         generic (
-            div     :     natural := 50000000
+            div : natural := 50000000
         );
         port (
-            clk_in  : in  std_logic; -- Entrada do clock div
-            clk_out : out std_logic  -- Saída do clock div
+            clk_in  : in  std_logic;
+            clk_out : out std_logic
         );
     end component CLKDIV;
-
 
     signal Kpress, Kscan, CLK_Divider : std_logic;
 
 begin
-    scan: component Key_Scan
+
+    Clk_div : component CLKDIV
+    generic map (50)
+    port map (
+        clk_in  => CLK,
+        clk_out => CLK_Divider
+    );
+
+    scan : component Key_Scan
     port map (
         RESET           => RESET,
         CLK             => CLK_Divider,
+        Kscan           => Kscan,
         Kpress          => Kpress,
         K               => K,
-        Kscan           => Kscan,
         Keys_Vertical   => Keys_Vertical,
         Keys_Horizontal => Keys_Horizontal
     );
 
-    control: component Key_Control
+    control : component Key_Control
     port map (
-        RESET           => RESET,
-        CLK             => CLK_Divider,
-        Kpress          => Kpress,
-        Kack            => Kack,
-        Tdelay          => Tdelay,
-        Kval            => Kval,
-        Kscan           => Kscan
+        RESET  => RESET,
+        CLK    => CLK,
+        Kpress => Kpress,
+        Kack   => Kack,
+        Tdelay => Tdelay,
+        Kval   => Kval,
+        Kscan  => Kscan
     );
-
-    Clk_div: component CLKDIV
-    generic map (
-        50
-    )
-    port map (
-        clk_in          => CLK,
-        clk_out         => CLK_Divider
-    );
-
 
 end architecture logicFunction;
