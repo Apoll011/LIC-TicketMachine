ff8df4967bf2e2405325557ec6d1ad41b5e98dbd
apoll011 <tiagorobotik@gmail.com>
2026-05-22 10:38:26 +0100

    refactor: reorganize App class methods and remove unused icon definitions

--- f2/src/App.kt
+++ f2/src/App.kt
@@ -3,14 +3,17 @@ import java.time.format.DateTimeFormatter
 
 
 class App {
+    var currentDestiny = 0
+
     fun init() {
         TUI.init()
         TicketDispenser.init()
+        Stations.init()
     }
 
     fun start() {
         firstScreen()
-        menu()
+        mainLoop()
     }
 
     fun firstScreen() {
@@ -24,11 +27,41 @@ class App {
         TUI.write(now.format(formatter))
     }
 
+    fun mainLoop() {
+        while (true) {
+            menu()
+        }
+    }
+
     fun menu(){
         val key = TUI.readKey()
         if (key != '\u0000'){
-            TUI.clear()
-            TUI.echo()
+            listDestinys()
         }
+    }
+
+    fun listDestinys() {
+        val station = Stations.getStation(currentDestiny)
+        if (station == null) return
+        printDestiny(station, true, true, 0)
+    }
+
+    fun printDestiny(station: Station, withId: Boolean, roundTrip: Boolean, avaliableCurrency: Int) {
+        TUI.write(station.name)
+        TUI.cursor(1, 0)
+        
+        if (withId) {
+            TUI.write(String.format("%02d", station))
+        }
+
+        TUI.writeIcon(Icons.UPWARDS_ARROW)
+        if (roundTrip) TUI.writeIcon(Icons.UPWARDS_ARROW)
+        
+        val p = (if (roundTrip) station.price * 2 else station.price) - avaliableCurrency
+
+        val price = String.format("%.2f", p / 100.0)
+        TUI.cursor(1, 11)
+        TUI.write(price)
+        TUI.writeIcon(Icons.EURO_SIGN)
     }
 }
