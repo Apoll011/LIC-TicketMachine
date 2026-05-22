ff72eea04fe0753e8d6094ad81a13513a16aee0f
Apoll0_11 <80171544+Apoll011@users.noreply.github.com>
2026-05-22 12:44:47 +0100

    Update App.kt

--- f2/src/App.kt
+++ f2/src/App.kt
@@ -29,7 +29,7 @@ class App {
 
         TUI.write(now.format(formatter))
         
-        while (TUI.getKey() != '\u0000'){
+        while (TUI.getKey() == '\u0000'){
             listDestinys()
         }
     }
